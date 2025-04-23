dc9dd9c4d93ad396a90a998f9f356e51c39b7ea6
Nic Bollis <nbollis@comcast.net>
2025-04-22 20:49:12 -0500

    Gamestate

---
+++ SolivtaireCore/Solitaire/GameState.cs
@@ -0,0 +1,62 @@
+﻿namespace SolivtaireCore;
+
+public class GameState
+{
+    public List<FoundationPile> FoundationPiles { get; } = new();
+    public List<TableauPile> TableauPiles { get; } = new();
+    public StockPile StockPile { get; } = new();
+    public WastePile WastePile { get; } = new();
+
+    public bool IsGameWon => FoundationPiles.All(pile => pile.Count == 13);
+
+    public GameState()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            FoundationPiles.Add(new FoundationPile((Suit)i));
+        }
+        for (int i = 0; i < 7; i++)
+        {
+            TableauPiles.Add(new TableauPile());
+        }
+    }
+
+    public void Reset()
+    {
+        foreach (var pile in FoundationPiles)
+        {
+            pile.Cards.Clear();
+        }
+        foreach (var pile in TableauPiles)
+        {
+            pile.Cards.Clear();
+        }
+        StockPile.Cards.Clear();
+        WastePile.Cards.Clear();
+    }
+
+    public void DealCards(StandardDeck deck)
+    {
+        deck.Shuffle();
+        Card[][] tableauCards = new Card[TableauPiles.Count][];
+        for (int i = 0; i < TableauPiles.Count; i++)
+        {
+            tableauCards[i] = new Card[i + 1];
+            for (int j = 0; j < tableauCards[i].Length; j++)
+            {
+                tableauCards[i][j] = deck.DrawCard();
+            }
+        }
+        for (int i = 0; i < TableauPiles.Count; i++)
+        {
+            // cards.addrange here instead of add range so we do not trigger the validity check on deal
+            TableauPiles[i].Cards.AddRange(tableauCards[i]);
+            TableauPiles[i].TopCard.IsFaceUp = true;
+        }
+
+        while (deck.Cards.Count > 0)
+        {
+            StockPile.AddCard(deck.DrawCard());
+        }
+    }
+}
---
+++ Test/GameStateTests.cs
@@ -0,0 +1,86 @@
+using SolivtaireCore;
+
+namespace Test;
+
+[TestFixture]
+public class GameStateTests
+{
+    [Test]
+    public void GameState_IsGameWon_AllFoundationPilesComplete_ShouldReturnTrue()
+    {
+        // Arrange  
+        var gameState = new GameState();
+        foreach (var pile in gameState.FoundationPiles)
+        {
+            for (int i = 1; i <= 13; i++)
+            {
+                pile.Cards.Add(new Card(pile.Suit, (Rank)i));
+            }
+        }
+
+        // Act  
+        var result = gameState.IsGameWon;
+
+        // Assert  
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void GameState_IsGameWon_NotAllFoundationPilesComplete_ShouldReturnFalse()
+    {
+        // Arrange  
+        var gameState = new GameState();
+        foreach (var pile in gameState.FoundationPiles)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                pile.Cards.Add(new Card(pile.Suit, (Rank)i));
+            }
+        }
+
+        // Act  
+        var result = gameState.IsGameWon;
+
+        // Assert  
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void GameState_Reset_ShouldClearAllPiles()
+    {
+        // Arrange  
+        var gameState = new GameState();
+        gameState.FoundationPiles[0].Cards.Add(new Card(Suit.Hearts, Rank.Ace));
+        gameState.TableauPiles[0].Cards.Add(new Card(Suit.Spades, Rank.King));
+        gameState.StockPile.Cards.Add(new Card(Suit.Clubs, Rank.Queen));
+        gameState.WastePile.Cards.Add(new Card(Suit.Diamonds, Rank.Jack));
+
+        // Act  
+        gameState.Reset();
+
+        // Assert  
+        Assert.That(gameState.FoundationPiles.All(pile => pile.Cards.Count == 0), Is.True);
+        Assert.That(gameState.TableauPiles.All(pile => pile.Cards.Count == 0), Is.True);
+        Assert.That(gameState.StockPile.Cards.Count, Is.EqualTo(0));
+        Assert.That(gameState.WastePile.Cards.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void GameState_DealCards_ShouldDistributeCardsCorrectly()
+    {
+        // Arrange  
+        var gameState = new GameState();
+        var deck = new StandardDeck();
+
+        // Act  
+        gameState.DealCards(deck);
+
+        // Assert  
+        for (int i = 0; i < gameState.TableauPiles.Count; i++)
+        {
+            Assert.That(gameState.TableauPiles[i].Cards.Count, Is.EqualTo(i + 1));
+            Assert.That(gameState.TableauPiles[i].TopCard.IsFaceUp, Is.True);
+        }
+        Assert.That(gameState.StockPile.Cards.Count, Is.EqualTo(52 - 28)); // 52 cards minus 28 dealt to tableau  
+    }
+}
