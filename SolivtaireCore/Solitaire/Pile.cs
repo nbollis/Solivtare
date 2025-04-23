e54355cb2fa4132bb0b4956306b37a42dbd712be
Nic Bollis <nbollis@comcast.net>
2025-04-22 20:29:57 -0500

    Piles for solitaire

---
+++ SolivtaireCore/Solitaire/Pile.cs
@@ -0,0 +1,89 @@
+﻿namespace SolivtaireCore;
+
+/// <summary>
+/// An abstract stack of cards
+/// </summary>
+public abstract class Pile
+{
+    /// <summary>
+    /// Represents a pile of cards in a solitaire game
+    /// </summary>
+    /// <remarks> Top card is the last card in the list. </remarks>
+    public List<Card> Cards { get; } = [];
+    public int Count => Cards.Count;
+    public bool IsEmpty => Count == 0;
+
+    public Card TopCard => IsEmpty ? throw new InvalidOperationException("Pile is empty.") : Cards[^1];
+    public Card BottomCard => IsEmpty ? throw new InvalidOperationException("Pile is empty.") : Cards[0];
+
+    protected Pile(IEnumerable<Card>? initialCards = null)
+    {
+        if (initialCards == null) return;
+        foreach (var card in initialCards)
+        {
+            Cards.Add(card);
+        }
+    }
+
+    //public abstract bool CanTakeCard();
+    //public abstract bool CanTakeCards(IEnumerable<Card> cards);
+
+    public abstract bool CanAddCard(Card card);
+
+    public void AddCard(Card card)
+    {
+        if (!CanAddCard(card))
+            throw new InvalidOperationException($"Cannot add {card} to this pile.");
+        Cards.Add(card);
+    }
+}
+
+/// <summary>
+/// Pile of cards for win condition, all cards of the same suit in ascending order.
+/// </summary>
+/// <param name="suit"></param>
+/// <param name="initialCards"></param>
+public class FoundationPile(Suit suit, IEnumerable<Card>? initialCards = null) : Pile(initialCards)
+{
+    public Suit Suit { get; } = suit;
+
+    public override bool CanAddCard(Card card)
+    {
+        if (IsEmpty)
+            return card.Rank == Rank.Ace && card.Suit == Suit;
+        return card.Suit == Suit && card.Rank == TopCard.Rank + 1;
+    }
+}
+
+/// <summary>
+/// Pile of cards that can be played on (columns in solitaire).
+/// </summary>
+/// <param name="initialCards"></param>
+public class TableauPile(IEnumerable<Card>? initialCards = null) : Pile(initialCards)
+{
+    public override bool CanAddCard(Card card)
+    {
+        if (IsEmpty)
+            return card.Rank == Rank.King;
+        return card.Color != TopCard.Color && card.Rank == TopCard.Rank - 1;
+    }
+}
+
+/// <summary>
+/// Deck which playable cards are drawn from.
+/// </summary>
+/// <param name="initialCards"></param>
+public class StockPile(IEnumerable<Card>? initialCards = null) : Pile(initialCards)
+{
+    public override bool CanAddCard(Card card) => true; // Stock pile can accept any card
+}
+
+
+/// <summary>
+/// Top card only can be used for play
+/// </summary>
+/// <param name="initialCards"></param>
+public class WastePile(IEnumerable<Card>? initialCards = null) : Pile(initialCards)
+{
+    public override bool CanAddCard(Card card) => true; // Waste pile can accept any card
+}
---
+++ Test/PileTests.cs
@@ -0,0 +1,160 @@
+using SolivtaireCore;
+
+namespace Test
+{
+    namespace Pile
+    {
+        [TestFixture]
+        public class PileTests
+        {
+            [Test]
+            public void Pile_TopCard_EmptyPile_ShouldThrowInvalidOperationException()
+            {
+                // Arrange  
+                var pile = new TableauPile();
+
+                // Act & Assert  
+                Assert.That(() => { var topCard = pile.TopCard; }, Throws.InvalidOperationException);
+            }
+
+            [Test]
+            public void Pile_AddCard_InvalidCard_ShouldThrowInvalidOperationException()
+            {
+                // Arrange  
+                var pile = new FoundationPile(Suit.Clubs);
+                var card = new Card(Suit.Spades, Rank.Two);
+
+                // Act & Assert  
+                Assert.That(() => pile.AddCard(card), Throws.InvalidOperationException);
+            }
+        }
+
+
+        [TestFixture]
+        public class FoundationPileTests
+        {
+            [Test]
+            public void CanAddCard_EmptyPile_ShouldAllowAceOfSameSuit()
+            {
+                // Arrange  
+                var pile = new FoundationPile(Suit.Hearts);
+                var card = new Card(Suit.Hearts, Rank.Ace);
+
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.True);
+            }
+
+            [Test]
+            public void CanAddCard_NonEmptyPile_ShouldAllowNextRankOfSameSuit()
+            {
+                // Arrange  
+                var pile = new FoundationPile(Suit.Hearts);
+                pile.AddCard(new Card(Suit.Hearts, Rank.Ace));
+                var card = new Card(Suit.Hearts, Rank.Two);
+
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.True);
+            }
+
+            [Test]
+            public void CanAddCard_WrongSuit_ShouldNotAllowCard()
+            {
+                // Arrange  
+                var pile = new FoundationPile(Suit.Hearts);
+                var card = new Card(Suit.Spades, Rank.Ace);
+
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.False);
+            }
+
+            [Test]
+            public void CanAddCard_WrongRank_ShouldNotAllowCard()
+            {
+                // Arrange  
+                var pile = new FoundationPile(Suit.Hearts);
+                pile.AddCard(new Card(Suit.Hearts, Rank.Ace));
+                var card = new Card(Suit.Hearts, Rank.Three);
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.False);
+            }
+        }
+
+        [TestFixture]
+        public class StockPileTests
+        {
+            [Test]
+            public void StockPile_CanAddCard_ShouldAlwaysAllowCard()
+            {
+                // Arrange  
+                var pile = new StockPile();
+                var card = new Card(Suit.Clubs, Rank.Ten);
+
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.True);
+            }
+        }
+
+        [TestFixture]
+        public class TableauPileTests
+        {
+            [Test]
+            public void CanAddCard_EmptyPile_ShouldAllowKing()
+            {
+                // Arrange  
+                var pile = new TableauPile();
+                var card = new Card(Suit.Spades, Rank.King);
+
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.True);
+            }
+
+            [Test]
+            public void CanAddCard_EmptyPile_ShouldNotAllowNonKing()
+            {
+                // Arrange  
+                var pile = new TableauPile();
+                var card = new Card(Suit.Spades, Rank.Queen);
+
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.False);
+            }
+
+            [Test]
+            public void CanAddCard_NonEmptyPile_ShouldAllowOppositeColorAndLowerRank()
+            {
+                // Arrange  
+                var pile = new TableauPile([new Card(Suit.Hearts, Rank.Queen)]);
+                var card = new Card(Suit.Spades, Rank.Jack);
+
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.True);
+            }
+
+            [Test]
+            public void CanAddCard_SameColor_ShouldNotAllowCard()
+            {
+                // Arrange  
+                var pile = new TableauPile([new Card(Suit.Hearts, Rank.Queen)]);
+                var card = new Card(Suit.Diamonds, Rank.Jack);
+
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.False);
+            }
+        }
+
+        [TestFixture]
+        public class WastePileTests
+        {
+            [Test]
+            public void WastePile_CanAddCard_ShouldAlwaysAllowCard()
+            {
+                // Arrange  
+                var pile = new WastePile();
+                var card = new Card(Suit.Diamonds, Rank.Five);
+
+                // Act & Assert  
+                Assert.That(pile.CanAddCard(card), Is.True);
+            }
+        }
+    }
+}
